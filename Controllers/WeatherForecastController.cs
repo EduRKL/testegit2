c08bdd7abdfee5d9ce5f9dfc91757009adee10e1
Eduardo Lima <106208300+EduRKL@users.noreply.github.com>
2023-03-26 00:37:53 -0300

    hotfix put

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -18,8 +18,8 @@ namespace testegit.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpPut(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Put()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
